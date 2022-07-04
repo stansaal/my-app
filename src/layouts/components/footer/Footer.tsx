1b14e2dcd066ccf2829ad9f4b5013e19e5901625
Stan Saal <stanislav.saal@ya.ru>
2022-07-04 16:28:19 +0300

    (task-41) create footer component

--- src/layouts/components/footer/Footer.tsx
+++ src/layouts/components/footer/Footer.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="container">
+			<div className="footer">
+				<p className="pull-left">Copyright &copy; 2017 Your Website Name</p>
+				<p className="pull-right">
+					Mediumish Theme by{" "}
+					<a target="_blank" href="https://www.wowthemes.net" rel="noreferrer">
+						WowThemes.net
+					</a>
+				</p>
+				<div className="clearfix"></div>
+			</div>
+		</div>
+	);
 };
 
 export { Footer };
