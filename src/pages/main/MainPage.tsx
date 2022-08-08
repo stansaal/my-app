e20c0730d0c0dadf28129516dd05d18140586a9d
Stan Saal <stanislav.saal@ya.ru>
2022-08-08 22:10:01 +0300

    Modules type declarations

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "classnames";
 
 //layout
 import { MainLayout } from "../../layouts";
@@ -7,13 +8,23 @@ import { MainLayout } from "../../layouts";
 import FeaturedSection from "./sections/Featured";
 import RecentSection from "./sections/Recent";
 
+import { ReactComponent as PhoneSVG } from "../../assets/img/phone.svg";
+
+import PhoneSRC from "../../assets/img/phone.svg";
+
+import s from "./MainPage.module.scss";
+
 const MainPage = () => {
 	return (
 		<MainLayout>
 			<div className="container">
 				<div className="mainheading">
-					<h1 className="sitetitle">Mediumish</h1>
-					<p className="lead">Bootstrap theme, medium style, simply perfect for bloggers</p>
+					<h1 className={cn("sitetitle", s.heading)}>
+						Mediumish <PhoneSVG />
+					</h1>
+					<p className="lead">
+						Bootstrap theme, medium style, simply perfect for bloggers <img src={PhoneSRC} alt="" />
+					</p>
 				</div>
 				<FeaturedSection />
 				<RecentSection />
